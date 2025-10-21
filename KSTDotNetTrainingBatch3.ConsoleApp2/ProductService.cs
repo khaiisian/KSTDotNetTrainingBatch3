6160a05910c3999bb515a5481d3f4476671b30b4
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-10-21 23:31:11 +0630

    feat: Ado .Net Service [Complete]

---
+++ KSTDotNetTrainingBatch3.ConsoleApp2/ProductService.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSTDotNetTrainingBatch3.ConsoleApp2;
+
+public class ProductService
+{
+    SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = ".",
+        UserID = "sa",
+        Password = "sasa@123",
+        TrustServerCertificate = true,
+        InitialCatalog = "MiniPOS"
+    };
+
+    #region Read
+    public void Read()
+    {
+        string query = @"SELECT [ProductId]
+                      ,[ProductName]
+                      ,[Quantity]
+                      ,[Price]
+                      ,[DeleteFlag]
+                       FROM [dbo].[Tbl_Product]";
+
+        SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        adapter.Fill(dt);
+
+        foreach (DataRow dr in dt.Rows)
+        {
+            var price = Convert.ToDecimal(dr["Price"]);
+            Console.WriteLine("ProductId => " + dr["ProductId"].ToString());
+            Console.WriteLine("Product Name => " + dr["ProductName"].ToString());
+            Console.WriteLine("Quantity => " + dr["Quantity"].ToString());
+            Console.WriteLine("Price => " + Convert.ToDecimal(dr["Price"]).ToString("n2"));
+            Console.WriteLine("Price0 => " +price.ToString("n"));
+        }
+    }
+    #endregion
+
+    #region Create
+    public void Create()
+    {
+        string query = @"INSERT INTO [dbo].[Tbl_Product]
+           ([ProductName]
+           ,[Quantity]
+           ,[Price]
+           ,[DeleteFlag])
+     VALUES
+           ('Apple'
+           ,20
+           ,2500
+           ,0)";
+
+        SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        var result = cmd.ExecuteNonQuery();
+
+        string message = result > 0 ? "Create Successful!" : "Create Failed!";
+        Console.WriteLine(message);
+    }
+    #endregion
+
+    #region Update
+    public void Update()
+    {
+        string query = @"UPDATE [dbo].[Tbl_Product]
+                       SET [ProductName] = 'Dragon Fruit'
+                          ,[Quantity] = 5
+                          ,[Price] = 3500
+                          ,[DeleteFlag] = 0
+                        WHERE ProductId = 3";
+        SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        var result = cmd.ExecuteNonQuery();
+        var message = result > 0 ? "Update Successful!" : "Update Failed";
+        Console.WriteLine(message);
+    }
+    #endregion
+
+    #region Delete
+    public void Delete()
+    {
+        string query = @"DELETE FROM [dbo].[Tbl_Product]
+      WHERE ProductId = 4";
+        SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        connection.Open();
+        SqlCommand cmd = new SqlCommand(query, connection);
+        var result = cmd.ExecuteNonQuery();
+        var message = result > 0 ? "Delete Successful!" : "Delete Failed!";
+        Console.WriteLine(message);
+    }
+    #endregion
+}
--- KSTDotNetTrainingBatch3.ConsoleApp2/Program.cs
+++ KSTDotNetTrainingBatch3.ConsoleApp2/Program.cs
@@ -1,41 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using KSTDotNetTrainingBatch3.ConsoleApp2;
 using Microsoft.Data.SqlClient;
 using System.Data;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
-{
-    DataSource = ".",
-    UserID = "sa",
-    Password = "sa@123",
-    TrustServerCertificate = true,
-    InitialCatalog = "MiniPOS"
-};
+#region ado.net Read ........
+//SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+//{
+//    DataSource = ".",
+//    UserID = "sa",
+//    Password = "sa@123",
+//    TrustServerCertificate = true,
+//    InitialCatalog = "MiniPOS"
+//};
 
+//SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
 
-SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+//connection.Open();
+//string query = @"SELECT [Id]
+//      ,[Name]
+//      ,[Email]
+//      ,[DateOfBirth]
+//      ,[Salary]
+//  FROM [dbo].[Tbl_Employee]";
 
-connection.Open();
-string query = @"SELECT [Id]
-      ,[Name]
-      ,[Email]
-      ,[DateOfBirth]
-      ,[Salary]
-  FROM [dbo].[Tbl_Employee]";
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
+//connection.Close();
 
-SqlCommand cmd = new SqlCommand(query, connection);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-DataTable dt = new DataTable();
-adapter.Fill(dt);
-connection.Close();
+//for (int i = 0; i < dt.Rows.Count; i++)
+//{
+//    var row = dt.Rows[i];
+//    var salary = Convert.ToDecimal(row["Salary"]);
+//    Console.WriteLine("Id => " + row["Id"]);
+//    Console.WriteLine("Name => " + row["Name"]);
+//    Console.WriteLine("Date Of Birth => " + row["DateOfBirth"]);
+//    Console.WriteLine("Salary => "+ salary.ToString("n0") +"Kyats");
+//}
+#endregion
 
-for (int i = 0; i < dt.Rows.Count; i++)
-{
-    var row = dt.Rows[i];
-    var salary = Convert.ToDecimal(row["Salary"]);
-    Console.WriteLine("Id => " + row["Id"]);
-    Console.WriteLine("Name => " + row["Name"]);
-    Console.WriteLine("Date Of Birth => " + row["DateOfBirth"]);
-    Console.WriteLine("Salary => "+ salary.ToString("n0") +"Kyats");
-}
+#region Ado.NET Service
+ProductService productService = new ProductService();
+//productService.Read();
+//productService.Create();
+//productService.Update();
+//productService.Delete();
+#endregion
+
